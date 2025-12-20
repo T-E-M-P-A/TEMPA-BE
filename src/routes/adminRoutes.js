ae84b9f26234a0c7894fdc8ef4d85dd19df55a68
Jojo297 <mafiffudin28@gmail.com>
2025-12-20 15:48:04 +0700

    add new endpoint api for get data program in admin route

--- src/routes/adminRoutes.js
+++ src/routes/adminRoutes.js
@@ -552,6 +552,55 @@ router.put(
   }
 );
 
+// get program campus for chart
+router.get(
+  "/get-program-campus-chart",
+  authenticateUser,
+  authorizeRoles(["admin"]),
+  async (req, res) => {
+    try {
+      const getProgramCampus = await prisma.program.findMany({
+        select: {
+          id: true,
+          program_name: true,
+          campus_program_id_campusTocampus: {
+            select: {
+              campus_name: true,
+            },
+          },
+          _count: {
+            select: {
+              mentee_progress: true,
+            },
+          },
+        },
+      });
+
+      // get count total mentee
+      const programsWithMenteeCount = getProgramCampus.map((program) => ({
+        id: program.id,
+        program_name: program.program_name,
+        campus_name: program.campus_program_id_campusTocampus?.campus_name,
+        // Total mentee diambil dari hasil perhitungan _count
+        total_mentee: program._count.mentee_progress,
+      }));
+
+      console.log(programsWithMenteeCount);
+
+      return res.status(200).json({
+        message: "Data program beserta total mentee berhasil diambil",
+        data: programsWithMenteeCount,
+      });
+    } catch (error) {
+      console.error(error);
+      return res.status(500).json({
+        message: "Terjadi kesalahan saat mengambil data program",
+        error: error.message,
+      });
+    }
+  }
+);
+
 // test midleware
 router.post(
   "/testing-midleware",
