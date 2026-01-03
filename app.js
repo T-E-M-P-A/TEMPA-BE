9e7d286972998e727dadbe691d08e62701626774
Jojo297 <mafiffudin28@gmail.com>
2026-01-04 01:10:50 +0700

    add rate limiter and helmet

--- app.js
+++ app.js
@@ -8,19 +8,42 @@ import campusRoutes from "./src/routes/campusRoute.js";
 import mentorRoutes from "./src/routes/mentorRoutes.js";
 import path from "path";
 import { fileURLToPath } from "url";
+import helmet from "helmet";
+import rateLimit from "express-rate-limit";
 
 const app = express();
+app.set("trust proxy", 1);
 const port = 8080;
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 // --- MIDDLEWARE ---
+app.use(helmet());
+
+const limiter = rateLimit({
+  windowMs: 15 * 60 * 1000,
+  limit: 100,
+  standardHeaders: "draft-7",
+  legacyHeaders: false,
+  message: {
+    status: 429,
+    message: "Terlalu banyak permintaan dari IP ini, silakan coba lagi nanti.",
+  },
+});
+
+app.use(limiter);
+app.use(
+  helmet({
+    crossOriginResourcePolicy: { policy: "cross-origin" },
+  })
+);
+
 app.use(morgan("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cors({ origin: "http://localhost:5173" }));
 
-app.use("/public", express.static(path.join(process.cwd(), "uploads"))); // <-- GUNAKAN process.cwd()
+app.use("/public", express.static(path.join(process.cwd(), "uploads")));
 
 // mentee route
 app.use("/api/v1", menteeRoutes);
